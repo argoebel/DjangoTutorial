e425e9c99423ede6bbc153c7c2400ceba64e4544
Austin Goebel <argoebel@ualberta.ca>
2019-09-07 15:08:26 -0600

    backend scraping initial

--- HelloWorldWebsite/home/models.py
+++ HelloWorldWebsite/home/models.py
@@ -7,3 +7,8 @@ from django.db import models
 
 class Counter(models.Model):
     count = models.IntegerField(blank=False, default=0)
+
+class Artist(models.Model):
+    id = models.CharField(max_length=1000, primary_key=True)
+    name = models.CharField(max_length=100)
+    followers = models.IntegerField()
--- HelloWorldWebsite/home/views.py
+++ HelloWorldWebsite/home/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from bs4 import BeautifulSoup
+from urllib.request import urlopen as uReq
+
 from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
@@ -17,7 +20,42 @@ class Home(generic.DetailView):
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
-        counter_object = Counter.objects.get(pk=1)
-        counter_object.count += 1
-        counter_object.save()
-        return redirect('homepage')
+
+
+        my_url = 'https://www.allmusic.com/search/artists/'
+
+        artist_name = "drake"
+
+        my_url+=artist_name
+
+        uClient = uReq(my_url)
+        page_html = uClient.read()
+        uClient.close()
+
+        #html parsing
+        page_soup = BeautifulSoup(page_html, "html.parser")
+
+        #grabs all products
+        containers = page_soup.findAll("ul", {"class":"search-results"})
+
+        #print("Newegg Video Cards\n")
+
+        for container in containers:
+            print(container)
+            #brand = container.find("div", "item-info")
+            #brand = brand.div.a.img["title"]
+            #print(brand)
+            #title_container = container.findAll("a", {"class":"item-title"})
+            #print(title_container)
+            #product_name = title_container[0].text
+            #print(product_name)
+            #shipping_container = container.findAll("li", {"class":"price-ship"})
+            #print(shipping_container)
+            #shipping = shipping_container[0].text.strip()
+            #print(shipping)
+            #print()
+
+            # counter_object = Counter.objects.get(pk=1)
+            # counter_object.count += 1
+            # counter_object.save()
+            return redirect('homepage')
