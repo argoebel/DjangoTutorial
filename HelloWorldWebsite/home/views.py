f4d54c1d9f19c24d5435aa46ca6b4193bf406bec
Austin Goebel <argoebel@ualberta.ca>
2019-09-08 09:45:26 -0600

    fixed artist name and added artist img link

--- HelloWorldWebsite/home/views.py
+++ HelloWorldWebsite/home/views.py
@@ -39,6 +39,12 @@ class Home(generic.DetailView):
         containers = page_soup.findAll("ul", {"class":"search-results"})
         artist_url = containers[0].div.a['href']
 
+        artist_name = page_soup.findAll("ul", {"class":"search-results"})[0].div.img['alt']
+        print(artist_name)
+
+        artist_img = page_soup.findAll("ul", {"class":"search-results"})[0].div.img['src']
+        print(artist_img)
+
         if artist_url[0:len(base_url)] == base_url:
             artist_url = artist_url[len('https://www.allmusic.com/artist/'):]
         print(artist_url)
